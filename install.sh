763fd152c0f46901ebeb9101389158a32e15031a
leo chen <38763092+leo03164@users.noreply.github.com>
2024-06-29 15:46:02 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# 安裝 zsh
+sudo apt update
+sudo apt install -y zsh
+
+# 安裝 tmux
+sudo apt install -y tmux
+
+# 安裝 net-tools
+sudo apt install -y net-tools
+
+# 安裝 htop
+sudo apt install -y htop
+
+# 安裝 wget
+sudo apt install -y wget
+
+# 安裝 curl
+sudo apt install -y curl
