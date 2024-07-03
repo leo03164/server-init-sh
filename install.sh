e7701eb5d855af18ab698edfa157f6cd6ca3439b
leo chen <38763092+leo03164@users.noreply.github.com>
2024-07-03 21:56:56 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # 安裝 zsh
 sudo apt update
 sudo apt install -y zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -y
 
 # 安裝 tmux
 sudo apt install -y tmux
