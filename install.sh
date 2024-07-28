eee2069317c104fe9ba7b31a694f944bec9d7080
leo chen <38763092+leo03164@users.noreply.github.com>
2024-07-28 09:10:02 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,11 +2,6 @@
 
 # 安裝 zsh
 sudo apt update
-sudo apt install -y zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -y
-
-# 安裝 zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 # 安裝 tmux
 sudo apt install -y tmux
@@ -25,3 +20,10 @@ sudo apt install -y vim
 
 # 安裝 git
 sudo apt install -y git
+
+# 安裝 zsh
+sudo apt install -y zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -y
+
+# 安裝 zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
