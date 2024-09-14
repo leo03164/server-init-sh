18a6baec9124a6da2801dd99dc6912f0d69200b3
leo chen <38763092+leo03164@users.noreply.github.com>
2024-09-14 11:25:38 +0800

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -16,3 +16,8 @@ echo \
 sudo apt-get update
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo usermod -aG docker $USER
+
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
